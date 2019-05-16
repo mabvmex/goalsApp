0e1872974896219aba899d9ee099682b7a65d885
mabvmex <mabvmex@gmail.com>
2019-05-16 16:23:01 -0500

    Implementacon del LOCALSTORAGE, carga y guardar

--- src/app/servicios/goals.service.ts
+++ src/app/servicios/goals.service.ts
@@ -11,10 +11,11 @@ export class GoalsService {
   constructor() {
     console.log('servico de LISTA inicializado');
 
-    const lista1 = new Lista ('Recolectar piedras del infinito');
-    const lista2 = new Lista ('Recuperar el trono de hierro');
+    this.cargarStorage();
+    // const lista1 = new Lista ('Recolectar piedras del infinito');
+    // const lista2 = new Lista ('Recuperar el trono de hierro');
 
-    this.listas.push(lista1, lista2);
+    // this.listas.push(lista1, lista2);
     // console.log(this.listas);
 
    }
@@ -22,6 +23,20 @@ export class GoalsService {
    crearLista(titulo: string) {
      const nuevaLista = new Lista (titulo);
      this.listas.push (nuevaLista);
+     this.guardarStorage();
+   }
+
+   guardarStorage() {
+     localStorage.setItem('data', JSON.stringify(this.listas));
+   }
+
+   cargarStorage() {
+     if (localStorage.getItem('data')){
+      this.listas = JSON.parse (localStorage.getItem('data'));
+     } // else {
+      // this.listas = []; // El ese está de más porque cuando se inicializa la lista ya es tipo arreglo.
+     // }
+     // Si no tenemos nada guardada en el storage, dara null, hay que validar
    }
 
 }
